5a166bc1cfeb1f1a14e23b65ec49b2964d2f723c
hyunjong <isc4614@naver.com>
2024-05-16 17:52:52 +0900

    tt

--- SSU_OS_login.c
+++ SSU_OS_login.c
@@ -3,101 +3,99 @@
 #include "user.h"
 #include "fcntl.h"
 
-/*
-Function Declaration
-*/
-void get_user_list();
-void parse_list();
-int check_idpw();
+#define INPUT_BUFFER 32
+#define MAX_ACCOUNT 10
 
-/*
-Variable Declaration
-*/
-char userID[16][32];
-char pwdID[16][32];
-int stop = 0;
-int count = 0; 
+char userID[MAX_ACCOUNT][INPUT_BUFFER];
+char pwdID[MAX_ACCOUNT][INPUT_BUFFER];
+int acc_cnt = 0;
+int stop_flag = 0;
 
-void get_user_list(){
-    int fd;
-
-    // list.txt open
-    fd = open("list.txt", O_RDONLY);
-    if(fd < 0){
-        printf(1, "open failed");
-        exit();
+void parse_word(int fd, char *dest)
+{
+  for (int j = 0;; j++)
+  {
+    if (read(fd, &dest[j], 1) <= 0) {
+      stop_flag = 1;
+      break;
     }
-
-    // list.txt에 있는 [Username] [Password] 가져오기
-    for(int i = 0; i < 10; i++){
-        // 종료 조건
-        if(stop){
-            break;
-        }
-        count++;
-
-        // save
-        parse_list(fd, userID[i]);
-        parse_list(fd, pwdID[i]);
+    if (dest[j] == ' ' || dest[j] == '\n') {
+      dest[j] = '\0';
+      break;
     }
+  }
 }
 
-void parse_list(int fd, char *ID){
-    for(int i = 0; ; i++){
-        if(read(fd, &ID[i], 1) <= 0){
-            stop = 1;
-            break;
-        }
-        if(ID[i] == ' ' || ID[i] == '\\n'){
-            ID[i] = '\\0';
-            break;
-        }
-    }
+void get_user_list()
+{
+  int fd;
+
+  fd = open("list.txt", O_RDONLY);
+  if (fd < 0)
+  {
+    printf(1, "opening list.txt has been error\n");
+    exit();
+  }
+
+  for (int i = 0; i < MAX_ACCOUNT; i++)
+  {
+    if(stop_flag)
+      break;
+    acc_cnt++;
+    parse_word(fd, userID[i]);
+    parse_word(fd, pwdID[i]);
+  }
+  close(fd);
 }
 
-int check_idpw(){
-    char id[32] = {0, };
-    char pw[32] = {0, };
+int check_idpw()
+{
+  char id[INPUT_BUFFER]={0,}, pw[INPUT_BUFFER] = {0, };
 
-    printf(1, "Username: ");
-    parse_list(0, id);
-    printf(1, "Password: ");
-    parse_list(0, pw);
+  printf(1, "Username: ");
+  parse_word(0, id); 
+  printf(1, "Password: ");
+  parse_word(0, pw);
 
-    for(int i = 0 ; i < count; i++){
-        if(!strcmp(id, userID[i])){
-            if(!strcmp(pw, pwdID[i])){
-                return 1;
-            }
-            return 0;
-        }
+  for (int i = 0; i < acc_cnt; i++)
+  {
+    if (!strcmp(id, userID[i]))
+    {
+      if (!strcmp(pw, pwdID[i]))
+      {
+        return 1;
+      }
+      return 0;
     }
-    return 0;
+  }
+  return 0;
 }
 
-int main(int argc, char *argv[]){
-    int pid, wpid;
-    get_user_list();
+int main(int argc, char *argv[])
+{
+  int pid, wpid;
+  get_user_list();
 
-    while(1){
-        if(check_idpw()){
-            for(;;){
-                printf(1, "init: starting sh\\n");
-                pid = fork();
-                if(pid < 0){
-                    printf(1, "init: fork failid\\n");
-                    exit();
-                }
-                if(pid == 0){
-                    exec("sh", argv);
-                    printf(1, "init: exec sh failed\\n");
-                    exit();
-                }
-                while((wpid = wait()) >= 0 && wpid != pid){
-                    printf(1, "zombie!\\n");
-                }
-
-            }
+  while (1)
+  {
+    if (check_idpw())
+      for (;;)
+      {
+        printf(1, "init: starting sh\n");
+        pid = fork();
+        if (pid < 0)
+        {
+          printf(1, "init: fork failed\n");
+          exit();
         }
-    }
+        if (pid == 0)
+        {
+          exec("sh", argv);
+          printf(1, "init: exec sh failed\n");
+          exit();
+        }
+        while ((wpid = wait()) >= 0 && wpid != pid)
+          printf(1, "zombie!\n");
+      }
+  }
 }
