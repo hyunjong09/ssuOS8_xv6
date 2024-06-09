5d6fdc88da76178273b637a80e060e57415c6541
hyunjong <isc4614@naver.com>
2024-06-10 03:53:48 +0900

    fault_test_final

--- fault_test2.c
+++ fault_test2.c
@@ -5,21 +5,32 @@
 
 #define PGSIZE 4096
 
+void test_address(char *addr);
+
 int
 main(int argc, char *argv[])
 {
-  char *valid_addr = sbrk(PGSIZE * 2); // 2 페이지 크기만큼 메모리를 할당합니다.
-  char *invalid_addr = valid_addr + (PGSIZE * 3);  // 할당된 메모리 영역 밖의 주소를 설정합니다.
+  if (argc != 2) {
+    printf(1, "Usage: pagefault_test <address>\n");
+    exit();
+  }
 
-  printf(1, "Accessing valid address: 0x%x\n", valid_addr);
-  valid_addr[0] = 'A';  // 정상적인 접근
+  uint addr = atoi(argv[1]);
 
-  printf(1, "Accessing invalid address: 0x%x\n", invalid_addr);
-  *invalid_addr = 'B';  // 페이지 폴트를 유발합니다.
-
-  // 페이지 폴트 후 정상적으로 실행될 것입니다.
-  printf(1, "Value at invalid address: %c\n", *invalid_addr);
-  printf(1, "This line should be printed.\n");
+  // 주소를 테스트합니다.
+  test_address((char*)addr);
 
   exit();
+}
+
+void test_address(char *addr)
+{
+  printf(1, "Accessing address: 0x%x\n", addr);
+
+  // 주소에 접근하여 페이지 폴트를 유발하거나 정상적으로 접근합니다.
+  *addr = 'A';
+
+  // 페이지 폴트가 처리된 후 정상적으로 실행됩니다.
+  printf(1, "Value at address: %c\n", *addr);
+  printf(1, "This line should be printed.\n");
 }
