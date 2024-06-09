f5f247244b042e04c8d47040ce16ca34428c03ff
hyunjong <isc4614@naver.com>
2024-06-10 03:50:21 +0900

    test2

--- fault_test2.c
+++ fault_test2.c
@@ -8,14 +8,18 @@
 int
 main(int argc, char *argv[])
 {
-  char *invalid_addr = (char*)(0x80000000);  // 커널 공간의 주소를 사용해 페이지 폴트를 유발합니다.
-  printf(1, "Accessing invalid address: 0x%x\n", invalid_addr);
-  
-  // 다음 줄에서 페이지 폴트가 발생할 것입니다.
-  printf(1, "Value at invalid address: %c\n", *invalid_addr);
+  char *valid_addr = sbrk(PGSIZE * 2); // 2 페이지 크기만큼 메모리를 할당합니다.
+  char *invalid_addr = valid_addr + (PGSIZE * 3);  // 할당된 메모리 영역 밖의 주소를 설정합니다.
 
-  // 프로그램이 여기까지 도달하지 못할 것입니다.
-  printf(1, "This line should not be printed.\n");
-  
+  printf(1, "Accessing valid address: 0x%x\n", valid_addr);
+  valid_addr[0] = 'A';  // 정상적인 접근
+
+  printf(1, "Accessing invalid address: 0x%x\n", invalid_addr);
+  *invalid_addr = 'B';  // 페이지 폴트를 유발합니다.
+
+  // 페이지 폴트 후 정상적으로 실행될 것입니다.
+  printf(1, "Value at invalid address: %c\n", *invalid_addr);
+  printf(1, "This line should be printed.\n");
+
   exit();
 }
