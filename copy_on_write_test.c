eb0cc431461d9b221a410c3c9331eee2d2397b6e
hyunjong <isc4614@naver.com>
2024-06-10 01:03:22 +0900

    user_mod

--- copy_on_write_test.c
+++ copy_on_write_test.c
@@ -1,24 +1,36 @@
+// copy_on_write_test.c
+
 #include "types.h"
 #include "stat.h"
 #include "user.h"
 
-int memory = 0;
+int main() {
+  printf(1, "before fork\n");
+  int num_free_pages_before = get_num_free_pages();
+  printf(1, "number of free pages: %d\n", num_free_pages_before);
 
-int main(int argc, char* argv[]) {
-    printf(1, "before fork\n");
-    printf(1, "number of free pages: %d\n\n", getNumFreePages());
+  int pid = fork();
 
-    if (fork() == 0) {
-        printf(1, "hi, i'm child process\n");
-        printf(1, "before write, number of free pages: %d\n", getNumFreePages());
-        memory = 3;
-        printf(1, "after write, number of free pages: %d\n", getNumFreePages());
-        printf(1, "bye\n");
+  if (pid < 0) {
+    printf(1, "fork failed\n");
+    exit();
+  } else if (pid == 0) {
+    // Child process
+    printf(1, "hi, i'm child process\n");
+    int num_free_pages_before_write = get_num_free_pages();
+    printf(1, "before write, number of free pages: %d\n", num_free_pages_before_write);
 
-        return 0;
-    }
+    // Trigger copy-on-write
+    char *ptr = sbrk(0);
+    ptr[0] = 'A';  // This should trigger a page fault and COW
 
-    while (1);
+    int num_free_pages_after_write = get_num_free_pages();
+    printf(1, "after write, number of free pages: %d\n", num_free_pages_after_write);
+    printf(1, "bye\n");
+  } else {
+    // Parent process
+    wait();
+  }
 
-    return 0;
+  exit();
 }
