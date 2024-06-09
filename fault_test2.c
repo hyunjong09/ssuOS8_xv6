776c1cae24c3786a4fb46cf299bd2f8cf6257a56
hyunjong <isc4614@naver.com>
2024-06-10 03:59:25 +0900

    test_modify

--- fault_test2.c
+++ fault_test2.c
@@ -2,23 +2,27 @@
 #include "stat.h"
 #include "user.h"
 #include "fcntl.h"
-
-#define PGSIZE 4096
-
+#include "memlayout.h"
 void test_address(char *addr);
 
 int
 main(int argc, char *argv[])
 {
-  if (argc != 2) {
-    printf(1, "Usage: pagefault_test <address>\n");
-    exit();
-  }
+  char *valid_addr = sbrk(PGSIZE * 2); // 2 페이지 크기만큼 메모리를 할당합니다.
+  char *page_fault_addr = valid_addr + (PGSIZE * 3);  // 할당된 메모리 영역 바로 밖의 주소를 설정합니다.
+  char *invalid_addr = (char*)0x80000000; // 커널 공간의 주소를 사용해 페이지 폴트를 유발합니다.
 
-  uint addr = atoi(argv[1]);
+  // 유효한 주소 테스트
+  printf(1, "Testing valid address:\n");
+  test_address(valid_addr);
 
-  // 주소를 테스트합니다.
-  test_address((char*)addr);
+  // 페이지 폴트를 유발하는 유효한 주소 테스트
+  printf(1, "Testing page fault address:\n");
+  test_address(page_fault_addr);
+
+  // 유효하지 않은 주소 테스트
+  printf(1, "Testing invalid address:\n");
+  test_address(invalid_addr);
 
   exit();
 }
